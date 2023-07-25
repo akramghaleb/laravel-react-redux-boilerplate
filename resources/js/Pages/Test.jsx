efea8bcf963f7d1a67154292b68547c60e761c66
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-25 13:06:05 +0300

    install redux

--- resources/js/Pages/Test.jsx
+++ resources/js/Pages/Test.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../store/counterSlice'
+import { Counter } from '../components/Counter';
 
 const Test = () => {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
+
     return (
-        <h1>This is test component</h1>
+        <>
+            <h1>This is test component</h1>
+            <Counter />
+        </>
     )
 }
 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {createInertiaApp } from '@inertiajs/inertia-react'
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import store from './store'
+import { Provider } from 'react-redux'
 
 createInertiaApp({
     // Below you can see that we are going to get all React components from resources/js/Pages folder
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`,import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        createRoot(el).render(
+            <Provider store={store}>
+                <App {...props} />
+            </Provider>
+        )
     },
 })
