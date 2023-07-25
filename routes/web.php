c91834813783f9b047c64ac088274953c8aa8a95
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-25 12:13:37 +0300

    Client-side setup

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Inertia\Inertia; // We are going to use this class to render React components
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Test'); // This will get component Test.jsx from the resources/js/Pages/Test.jsx
 });
